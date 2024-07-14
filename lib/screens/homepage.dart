ca6bb5d3f6251c0e126c99bb6426f1a43d0348b1
Rensith Udara Gonalagoda <105989347+RensithUdara@users.noreply.github.com>
2024-07-15 00:16:43 +0530

    Update UI

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,11 +1,11 @@
 import 'package:covid_app/services/covid_data.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _HomePageState createState() => _HomePageState();
 }
 
@@ -15,71 +15,114 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('COVID-19 Tracker'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.lightBlueAccent, Colors.white],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: const Text(
+              'ViroSafe - COVID-19 Tracker',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Card(
-                elevation: 5,
-                child: FutureBuilder(
-                  future: service.getCovidData(),
-                  builder: (context, snapshot) {
-                    Map<String, dynamic>? data = snapshot.data;
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.lightBlueAccent, Colors.white],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  elevation: 5,
+                  child: FutureBuilder(
+                    future: service.getCovidData(),
+                    builder: (context, snapshot) {
+                      Map<String, dynamic>? data = snapshot.data;
 
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: CircularProgressIndicator(),
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (snapshot.hasError) {
+                        return const Text(
+                          "Something went wrong...",
+                          style: TextStyle(fontSize: 18, color: Colors.red),
+                        );
+                      }
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ListTile(
+                            leading: const Icon(
+                              FontAwesomeIcons.viruses,
+                              size: 30,
+                              color: Colors.orange,
+                            ),
+                            title: const Text(
+                              "Local Total Cases",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            subtitle: Text(
+                              "${data!["local_total_cases"]}",
+                              style: const TextStyle(fontSize: 25),
+                            ),
+                          ),
+                          ListTile(
+                            leading: const Icon(
+                              FontAwesomeIcons.skullCrossbones,
+                              size: 30,
+                              color: Colors.red,
+                            ),
+                            title: const Text(
+                              "Local Deaths",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            subtitle: Text(
+                              "${data["local_deaths"]}",
+                              style: const TextStyle(fontSize: 25),
+                            ),
+                          ),
+                          ListTile(
+                            leading: const Icon(
+                              FontAwesomeIcons.heartbeat,
+                              size: 30,
+                              color: Colors.green,
+                            ),
+                            title: const Text(
+                              "Local Recovered",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            subtitle: Text(
+                              "${data["local_recovered"]}",
+                              style: const TextStyle(fontSize: 25),
+                            ),
+                          ),
+                        ],
                       );
-                    }
-                    if (snapshot.hasError) {
-                      return const Text("Something went wrong...",
-                          style: TextStyle(fontSize: 18, color: Colors.red));
-                    }
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          title: const Text(
-                            "Local Total Cases",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          subtitle: Text(
-                            "${data!["local_total_cases"]}",
-                            style: const TextStyle(fontSize: 25),
-                          ),
-                        ),
-                        ListTile(
-                          title: const Text(
-                            "Local Deaths",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          subtitle: Text(
-                            "${data["local_deaths"]}",
-                            style: const TextStyle(fontSize: 25),
-                          ),
-                        ),
-                        ListTile(
-                          title: const Text(
-                            "Local Recovered",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          subtitle: Text(
-                            "${data["local_recovered"]}",
-                            style: const TextStyle(fontSize: 25),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
